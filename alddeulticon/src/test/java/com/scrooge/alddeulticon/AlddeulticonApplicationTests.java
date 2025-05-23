7ff354fa1ad939f4cc765a0ef2bf0e13d7c7aa01
songyeongeun <ye3557@naver.com>
2025-05-23 14:31:06 +0900

    test: 기본 애플리케이션 테스트 클래스 추가

---
+++ alddeulticon/src/test/java/com/scrooge/alddeulticon/AlddeulticonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scrooge.alddeulticon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlddeulticonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
