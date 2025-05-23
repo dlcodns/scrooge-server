fff779a998c0e2d7528f2f535e8a3bb704a6d89c
songyeongeun <ye3557@naver.com>
2025-05-23 14:30:51 +0900

    feat: Spring Boot 메인 애플리케이션 및 AppConfig 설정

---
+++ alddeulticon/src/main/java/com/scrooge/alddeulticon/AlddeulticonApplication.java
@@ -0,0 +1,13 @@
+package com.scrooge.alddeulticon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlddeulticonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlddeulticonApplication.class, args);
+	}
+
+}
