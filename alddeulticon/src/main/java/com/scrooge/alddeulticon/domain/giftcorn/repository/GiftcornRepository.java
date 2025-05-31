fa5cd02f72d92bd3e574302040c31b0665065e96
Bak Hyeongwoo <toplionby421@gmail.com>
2025-05-31 23:14:45 +0900

    fix : 버그 수정 및 giftcorn 관련 데이터 삭제

--- alddeulticon/src/main/java/com/scrooge/alddeulticon/domain/giftcorn/repository/GiftcornRepository.java
+++
@@ -1,8 +0,0 @@
-package com.scrooge.alddeulticon.domain.giftcorn.repository;
-
-import com.scrooge.alddeulticon.domain.giftcorn.entity.Giftcorn;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GiftcornRepository extends JpaRepository<Giftcorn, Long> {
-    // 필요시 커스텀 메서드 추가
-}
